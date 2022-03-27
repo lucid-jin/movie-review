927cd4e427401957151381bdd80d4060008ef231
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-03-27 21:15:07 +0900

    Install: class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({})
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe())
   const port = process.env.PORT || 3000
   await app.listen(port);
 }
