e15446ea9b5190721563c9bd082f34ed31737398
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-03-31 22:42:37 +0900

    Add && install cors option

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import helmet from "helmet";
+import {Request} from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({})
+
   app.setGlobalPrefix('api');
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe())
+
+
+  const corsOptionsDelegate = (req: Request, callback) => {
+    const whitelist = [
+      'https://yaboja.netlify.app/login',
+      'http://localhost:4000'
+    ]
+
+    if (whitelist.includes(req.header('Origin'))){
+      callback(null, { origin: true})
+      return;
+    }
+    callback(null, { origin: false})
+  }
+  app.enableCors(corsOptionsDelegate)
   const port = process.env.PORT || 3000
   await app.listen(port);
 }
