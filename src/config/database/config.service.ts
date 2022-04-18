5e6059c4950821d971c0e31c92fb5e0594152545
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-04-18 20:28:51 +0900

    Change: db update sync false

--- src/config/database/config.service.ts
+++ src/config/database/config.service.ts
@@ -20,7 +20,7 @@ export class DBConfigService implements TypeOrmOptionsFactory {
       entities: ['dist/**/**/*.entity{.ts,.js}'],
       ssl: {rejectUnauthorized: isDevelopment},
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
       logging: false,
     };
   }
