436b0e51245e1b9fb5cc7d8763f420d779dff42c
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-05-06 20:41:23 +0900

    [BugFix] cors list fixed
    (cherry picked from commit ce92adf30d52bcad31197d710589eb451bd8b005)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ValidationPipe} from "@nestjs/common";
-import helmet from "helmet";
-import {Request} from "express";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {patchNestjsSwagger} from "@anatine/zod-nestjs";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { Request } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Movie Review')
@@ -25,19 +25,16 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   const corsOptionsDelegate = (req: Request, callback) => {
-    const whitelist = [
-      'https://yaboja.netlify.app/login',
-      'http://localhost:4000'
-    ]
+    const whitelist = ['https://yaboja.netlify.app', 'http://localhost:4000'];
 
-    if (whitelist.includes(req.header('Origin'))){
-      callback(null, { origin: true})
+    if (whitelist.includes(req.header('Origin'))) {
+      callback(null, { origin: true });
       return;
     }
-    callback(null, { origin: false})
-  }
-  app.enableCors(corsOptionsDelegate)
-  const port = process.env.PORT || 3000
+    callback(null, { origin: false });
+  };
+  app.enableCors(corsOptionsDelegate);
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 
